f3bbd2d260e3bbaf737caf28ca1a3f95beb85a10
mohamuhsin <muhsinohoho@outlook.com>
2024-09-30 17:38:56 +0300

    brought in the events.js code for fetching events, then used our loader property to fetch events data from our server got rid of unneccesary data

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -40,7 +40,20 @@ function App() {
           path: "events",
           element: <EventLayout />,
           children: [
-            { index: true, element: <EventsPage /> },
+            {
+              index: true,
+              element: <EventsPage />,
+              loader: async () => {
+                const response = await fetch("http://localhost:8080/events");
+
+                if (!response.ok) {
+                  //...
+                } else {
+                  const resData = await response.json();
+                  return resData.events;
+                }
+              },
+            },
             { path: ":id", element: <EventDetailPage /> },
             { path: "new", element: <NewEventPage /> },
             { path: ":id/edit", element: <EditEventPage /> },
