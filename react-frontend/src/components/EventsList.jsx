996e630a63ee6d2d01638a3f7907615310151464
mohamuhsin <muhsinohoho@outlook.com>
2024-10-02 15:00:51 +0300

    where not to use useLoaderData()

--- react-frontend/src/components/EventsList.jsx
+++ react-frontend/src/components/EventsList.jsx
@@ -1,8 +1,9 @@
-import { useLoaderData } from "react-router-dom";
+/* eslint-disable react/prop-types */
+//import { useLoaderData } from "react-router-dom";
 import classes from "./EventsList.module.css";
 
-function EventsList() {
-  const events = useLoaderData();
+function EventsList({ events }) {
+  // const events = useLoaderData();
   return (
     <div className={classes.events}>
       <h1>All Events</h1>
